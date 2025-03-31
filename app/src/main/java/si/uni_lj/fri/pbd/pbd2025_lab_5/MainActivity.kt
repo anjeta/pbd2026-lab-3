d823f817c325cc620f3c3bbb103e0fa968dcefe2
Octavian Machidon <octavian@Octavians-MacBook-Pro.local>
2025-03-31 13:42:29 +0200

    added missing code

--- app/src/main/java/si/uni_lj/fri/pbd/pbd2025_lab_5/MainActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/pbd2025_lab_5/MainActivity.kt
@@ -3,10 +3,16 @@ package si.uni_lj.fri.pbd.pbd2025_lab_5
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val NUM_OF_TABS = 3
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        configureTabLayout()
+    }
+
+
+
+
+    private fun configureTabLayout() {
+
+        // TODO: Implement tab-viewpager-adapter connection
+
     }
 }
